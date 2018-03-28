15d43b508c1b4aa719226551ffd55cd179173798
Pete <peterwalsh86@hotmail.com>
2018-03-28 20:08:31 +0100

    Update main.js

--- Pomodoro Clock/main.js
+++ Pomodoro Clock/main.js
@@ -14,40 +14,43 @@ var breakDecrement = document.querySelector('#breakDecrement');
 
 var start = document.querySelector('#start');
 var stop = document.querySelector('#stop');
+var reset = document.querySelector('#reset');
+
+
+sessionTime.innerHTML = sessionLength;
+breakTime.innerHTML = breakLength;
 
 
 
 
 function increment(time) {
-    console.log("...........................");
-    console.log("increment function start...");
-    console.log("time is: ", time);
-    console.log("time innerHtml is:", time.innerHTML);
+    // console.log("...........................");
+    // console.log("increment function start...");
+    // console.log("time is: ", time);
+    // console.log("time innerHtml is:", time.innerHTML);
     var timer = time.innerHTML
     if (timer >= 0 && timer < 59) {
         timer++;
-        console.log("timer is: ", timer);
+        // console.log("timer is: ", timer);
         time.innerHTML = timer;
-        console.log("time innerHtml is:", time.innerHTML);
+        // console.log("time innerHtml is:", time.innerHTML);
     }
-    console.log("increment function end...");
-
+    // console.log("increment function end...");
 }
 
 function decrement(time) {
-    console.log("...........................");
-    console.log("decrement function start");
-    console.log("time is: ", time);
-    console.log("time innerHtml is:", time.innerHTML);
+    // console.log("...........................");
+    // console.log("decrement function start");
+    // console.log("time is: ", time);
+    // console.log("time innerHtml is:", time.innerHTML);
     var timer = time.innerHTML
     if (timer > 0 && timer <= 59) {
         timer--;
-        console.log("timer is: ", timer);
+        // console.log("timer is: ", timer);
         time.innerHTML = timer;
-        console.log("time innerHtml is:", time.innerHTML);
-
+        // console.log("time innerHtml is:", time.innerHTML);
     }
-    console.log("decrement function end...");
+    // console.log("decrement function end...");
 }
 
 
@@ -74,38 +77,75 @@ function displayTime(counter) {
 
 }
 
-
-function toggleTimer(time) {
-   // find time now
-   inProgress = !inProgress;
-   var counter = time * 60;
-   var breakCounter = breakTime * 60;
-
-    if(inProgress) {
-        setInterval(function() {
-            if(counter > 0) {
-                counter--;
-                console.log(counter);
-                console.log(displayTime(counter));
-                sessionTime.innerHTML = displayTime(counter);
-            } else if (counter === 0) {
+function startTimer(time) {
+    // find time now
+    inProgress = true;
+    var workCounter = time * 60;
+    var breakCounter = breakTime * 60;
+ 
+     
+    setInterval(function() {
+        if(inProgress) {
+            if(workCounter > 0) {
+                workCounter--;
+                console.log("workCounter is: ", workCounter);
+                console.log(displayTime(workCounter));
+                sessionTime.innerHTML = displayTime(workCounter);
+            } else if (workCounter <= 0) {
+                console.log("else if");
+                console.log("breakCounter is: ", breakCounter);
                 if(breakCounter > 0) {
                     breakCounter--;
                     console.log(breakCounter);
                     console.log(displayTime(breakCounter));
                     breakTime.innerHTML = displayTime(breakCounter);
+                } else {
+                    inProgress = !inProgress;
                 }
             }
-        }, 1000);
-    } 
-    
-
-   // countdown
+        }
+    }, 1000);
 }
 
-//function stopTimer() {
+function stopTimer() {
+    inProgress = false;
+}
 
-//}
+function resetTimer() {
+    inProgress = false;
+    sessionTime.innerHTML = sessionLength;
+    breakTime.innerHTML = breakLength;
+}
+
+// function toggleTimer(time) {
+//    // find time now
+//    inProgress = !inProgress;
+//    var workCounter = time * 60;
+//    var breakCounter = breakTime * 60;
+
+//     if(inProgress) {
+//         setInterval(function() {
+//             if(workCounter > 0) {
+//                 countdown(workCounter, sessionTime);
+//             } else if (workCounter <= 0) {
+//                 if(breakCounter > 0) {
+//                     countdown(breakCounter, breakTime)
+//                 } else {
+//                     inProgress = !inProgress;
+//                 }
+//             }
+//         }, 100);
+//     } 
+// }
+
+// function countdown(counter, selection) {
+//     counter--;
+//     console.log(counter);
+//     console.log(displayTime(counter));
+//     counter.innerHTML = displayTime(counter);
+// }
+
+
 
 breakDecrement.addEventListener('click', function () {
     decrement(breakTime);
@@ -123,8 +163,8 @@ sessionIncrement.addEventListener('click', function () {
     increment(sessionTime);
 });
 
-start.addEventListener('click', function() {
-    toggleTimer(sessionLength);
-});
+start.addEventListener('click', () => startTimer(sessionLength));
 
-//stop.addEventListener('click', stopTimer);
+stop.addEventListener('click', () => stopTimer(breakLength));
+
+reset.addEventListener('click', () => resetTimer(breakLength));
